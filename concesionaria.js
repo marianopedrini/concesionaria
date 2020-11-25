65986389db37688cd09bf3af051768d258c21e3e
marianopedrini <pedrini.mariano96@gmail.com>
2020-11-25 14:30:43 -0300

    funcion venderAuto added

--- concesionaria.js
+++ concesionaria.js
@@ -2,7 +2,16 @@ const autos = require("./autos");
 
 let concesionaria = {
   autos: autos,
+
   buscarAuto: function (patente) {
     return this.autos.find((auto) => auto.patente == patente) || null;
   },
+
+  venderAuto: function (patente) {
+    let encontrado = this.buscarAuto(patente);
+    encontrado.vendido = true;
+    return this.buscarAuto(patente);
+  },
 };
+
+console.log(concesionaria.venderAuto("APL123"));
